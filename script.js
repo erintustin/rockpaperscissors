14ce66ab9f405f1414bf8f26e394134519e06e6c
Erin Tustin <erintustin@gmail.com>
2022-05-19 12:03:32 -0400

    Add user input and results functionality

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
 const rps = ["rock", "paper", "scissors"];
+let userSelection;
+let computerSelection;
 
 function computerPlay() {
     let rpsIndex = Math.floor(Math.random() * 3);
-    let computerSelection = rps[rpsIndex];
+    computerSelection = rps[rpsIndex];
     console.log(computerSelection);
+    userPlay();
+}
 
-    let playerSelection = prompt('Rock, paper, scissors, SHOOT:');
-    playerSelection.toLowerCase();
-    if (computerSelection = playerSelection) {
-        alert('We both threw ' + computerSelection + '. Best 2/3?');
-        
+function userPlay() {
+    userSelection = prompt('Rock, paper, scissors, SHOOT:');
+    userSelection.toLowerCase();
+
+    if (computerSelection == userSelection) {
+        alert('We both threw ' + computerSelection + '.');
+    } else if (computerSelection == "rock" && userSelection == "scissors") {
+        alert ("Rock crushes scissors! You Lose!");
+    } else if (computerSelection == "rock" && userSelection == "paper") {
+        alert("Paper covers rock! You Win!");
+    } else if (computerSelection == "paper" && userSelection == "scissors") {
+        alert("Scissors cut paper! You Win!");
+    } else if (computerSelection == "paper" && userSelection == "rock") {
+        alert("Paper covers rock! You Lose!");
+    } else if (computerSelection == "scissors" && userSelection == "rock") {
+        alert("Rock crushes scissors! You Win!");
+    } else if (computerSelection == "scissors" && userSelection == "paper") {
+        alert("Scissors cut paper! You Lose!");
+    } else {
+        alert ("That is not a rock, paper, or scissors");
     }
-
 }
